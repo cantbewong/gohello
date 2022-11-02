03de4297e14199de6552e9809385a4dbf8f5e026
cantbewong <wongsteven@vmware.com>
2022-11-02 12:42:19 -0700

    Create initial version of hello-world app

---
+++ hello-world.go
@@ -0,0 +1,14 @@
+// go run hello-world.go
+/// or go build hello-world.go
+
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func main() {
+  fmt.Println("Hello, Steve")
+  fmt.Println("The current time is ",time.Now()) 
+}
